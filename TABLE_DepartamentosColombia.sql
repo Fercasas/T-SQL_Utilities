80e3b9e858bc68131fbee06333b78427d9a3c973
Fercasas <44042157+Fercasas@users.noreply.github.com>
2020-04-01 21:35:04 -0500

    Update TABLE_DepartamentosColombia.sql

--- TABLE_DepartamentosColombia.sql
+++ TABLE_DepartamentosColombia.sql
@@ -1,3 +1,5 @@
+GO
+
 /**********************************************************************************************
 * TABLE: [DBO].[Departamentos]
 * CREATION DATE: April/01/2020
@@ -8,56 +10,83 @@
 * FIXED: 
 */
 
-IF OBJECT_ID('dbo.Departamentos', 'U') IS NULL
-BEGIN
-	CREATE TABLE Departamentos
-	(
-		 Id        INT IDENTITY(1, 1) PRIMARY KEY
-	   , Codigo    TINYINT NOT NULL
-	   , Nombre    VARCHAR(100) NOT NULL
-	)
+SET NOCOUNT ON
 
-	INSERT INTO              Departamentos(
-	Codigo
-  , Nombre)
-	SELECT     
-	Codigo
-  , Nombre
-	FROM(VALUES (
-		'05', 'ANTIOQUIA'), (
-		'08', 'ATLÁNTICO'), (
-		'11', 'BOGOTÁ, D.C.'), (
-		'13', 'BOLÍVAR'), (
-		'15', 'BOYACÁ'), (
-		'17', 'CALDAS'), (
-		'18', 'CAQUETÁ'), (
-		'19', 'CAUCA'), (
-		'20', 'CESAR'), (
-		'23', 'CÓRDOBA'), (
-		'25', 'CUNDINAMARCA'), (
-		'27', 'CHOCÓ'), (
-		'41', 'HUILA'), (
-		'44', 'LA GUAJIRA'), (
-		'47', 'MAGDALENA'), (
-		'50', 'META'), (
-		'52', 'NARIÑO'), (
-		'54', 'NORTE DE SANTANDER'), (
-		'63', 'QUINDÍO'), (
-		'66', 'RISARALDA'), (
-		'68', 'SANTANDER'), (
-		'70', 'SUCRE'), (
-		'73', 'TOLIMA'), (
-		'76', 'VALLE DEL CAUCA'), (
-		'81', 'ARAUCA'), (
-		'85', 'CASANARE'), (
-		'86', 'PUTUMAYO'), (
-		'88', 'ARCHIPIÉLAGO DE SAN ANDRÉS, PROVIDENCIA Y SANTA CATALINA'), (
-		'91', 'AMAZONAS'), (
-		'94', 'GUAINÍA'), (
-		'95', 'GUAVIARE'), (
-		'97', 'VAUPÉS'), (
-		'99', 'VICHADA') ) AS X(Codigo, Nombre)
-END
+BEGIN TRY
 
+	IF OBJECT_ID('dbo.Departamentos', 'U') IS NULL
+	BEGIN
+		CREATE TABLE Departamentos
+		(
+			 Id        INT IDENTITY(1, 1)
+		   , Codigo    VARCHAR(2)
+			 PRIMARY KEY
+		   , Nombre    VARCHAR(100) NOT NULL
+		)
+		PRINT '|->	Se ha creado la tabla dbo.Departamentos.'
+	END
+END TRY
+BEGIN CATCH
+	PRINT '|->	Error creando tabla dbo.Departamentos: ' + ERROR_MESSAGE()
+END CATCH
 
+BEGIN TRY
+	IF OBJECT_ID('dbo.Departamentos', 'U') IS NULL
+	BEGIN
+		PRINT '|->	No existe la tabla dbo.Departamentos y no se pueden insertar los registros.'
+	END
+		ELSE
+	BEGIN
 
+		INSERT INTO              Departamentos(
+		Codigo
+	  , Nombre)
+		SELECT       
+		X.Codigo
+	  , X.Nombre
+		FROM  (VALUES (
+			'05', 'ANTIOQUIA'), (
+			'08', 'ATLÁNTICO'), (
+			'11', 'BOGOTÁ, D.C.'), (
+			'13', 'BOLÍVAR'), (
+			'15', 'BOYACÁ'), (
+			'17', 'CALDAS'), (
+			'18', 'CAQUETÁ'), (
+			'19', 'CAUCA'), (
+			'20', 'CESAR'), (
+			'23', 'CÓRDOBA'), (
+			'25', 'CUNDINAMARCA'), (
+			'27', 'CHOCÓ'), (
+			'41', 'HUILA'), (
+			'44', 'LA GUAJIRA'), (
+			'47', 'MAGDALENA'), (
+			'50', 'META'), (
+			'52', 'NARIÑO'), (
+			'54', 'NORTE DE SANTANDER'), (
+			'63', 'QUINDÍO'), (
+			'66', 'RISARALDA'), (
+			'68', 'SANTANDER'), (
+			'70', 'SUCRE'), (
+			'73', 'TOLIMA'), (
+			'76', 'VALLE DEL CAUCA'), (
+			'81', 'ARAUCA'), (
+			'85', 'CASANARE'), (
+			'86', 'PUTUMAYO'), (
+			'88', 'ARCHIPIÉLAGO DE SAN ANDRÉS, PROVIDENCIA Y SANTA CATALINA'), (
+			'91', 'AMAZONAS'), (
+			'94', 'GUAINÍA'), (
+			'95', 'GUAVIARE'), (
+			'97', 'VAUPÉS'), (
+			'99', 'VICHADA') ) AS X(Codigo, Nombre)
+		LEFT OUTER JOIN dbo.Departamentos AS DE
+			  ON X.Codigo = DE.Codigo
+		WHERE DE.Id IS NULL
+
+		PRINT '|->	Registros insertados en la tabla dbo.Departamentos: ' + CAST(@@ROWCOUNT AS VARCHAR)
+	END
+END TRY
+BEGIN CATCH
+	PRINT '|->	Error insertando en tabla dbo.Departamentos: ' + ERROR_MESSAGE()
+END CATCH
+
+GO
