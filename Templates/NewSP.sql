15f840a5f30a73e61a655ddb53983d1f65bdaf4a
Fernando Casas <fercasas@hotmail.com>
2021-10-29 08:39:38 -0500

    Add NewSP template

---
+++ Templates/NewSP.sql
@@ -0,0 +1,44 @@
+--SCRIPT START
+
+GO
+
+PRINT '>>>	CREATE OR ALTER PROCEDURE dbo.USP_XXXXXX'
+
+GO
+
+CREATE OR ALTER PROCEDURE dbo.USP_XXXXXX 
+	--Parameters
+	@Debug    BIT = 0
+AS
+/*
+	Store Procedure that .....
+
+	Parameters	:
+	@Debug		: Print or not messages.
+
+
+	Execution:
+
+
+	AUTHOR: 
+*/
+BEGIN
+	SET NOCOUNT ON
+	DECLARE 
+	@Message         VARCHAR(MAX) = NULL, 
+	@ErrorMessage    VARCHAR(MAX)
+
+	BEGIN TRY
+		SET @Message = '-->	Start USP_XXXXXX'
+		IF @Debug = 1
+			PRINT @Message
+
+	END TRY
+	BEGIN CATCH
+		SET @ErrorMessage = 'ERROR dbo.USP_XXXXXX [' + @Message + ']:' + COALESCE(ERROR_MESSAGE(), '')
+		RAISERROR(@ErrorMessage, 16, 1)
+	END CATCH
+END
+
+GO
+--END OF SCRIPT
